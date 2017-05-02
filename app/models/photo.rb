6d47d82c21568ab953a0fc11bd69554b0e0224a6
Jerome Pratt <jerome.pratt@outlook.com>
2017-05-02 20:37:04 +0100

    Photo.for_user returns photos which don't belong to the user passed in

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,4 +6,6 @@ class Photo < ApplicationRecord
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] },
     size: { in: 0..10.megabytes }
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"]
+
+  scope :for_user, ->(user) { where.not(user_id: user.id)}
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Photo do
   }
   it { is_expected.to validate_attachment_size(:image)
     .less_than(10.megabytes) }
+
+  describe '.for_user' do
+    context 'returning photos from different users' do
+      let(:first_user) { create(:user) }
+      let(:first_user_photo) { create(:photo, user: first_user) }
+      let(:second_user) { create(:user) }
+      let(:second_user_photo) { create(:photo, user: second_user) }
+
+      it 'should return photos from different users' do
+        expect(Photo.for_user(first_user)).to include second_user_photo
+      end
+
+      it 'should not return photos belonging to the user' do
+        expect(Photo.for_user(first_user)).not_to include first_user_photo
+      end
+    end
+  end
 end
