6abb4232b69c54e7ca2d5568d4e9d1b6ad014088
Jerome Pratt <jerome.pratt@outlook.com>
2017-05-04 19:37:43 +0100

    Add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:sessions]
+  
+  #change devise routes
   as :user do
     get 'signin', to: 'devise/sessions#new', as: :new_user_session
     post 'signin', to: 'devise/sessions#create', as: :user_session
     delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
+
   root to: 'photos#index'
   resources :photos, only: :index do
     get :for_voting, on: :collection
