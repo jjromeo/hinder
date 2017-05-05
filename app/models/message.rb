32bbe77f48ce1d93635a241fd630df3e7538a7d0
Jerome Pratt <jerome.pratt@outlook.com>
2017-05-05 11:07:14 +0100

    Publish broadcast message in app as quickfix

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,9 +5,18 @@ class Message < ApplicationRecord
   validates_presence_of :content
   validates_length_of :content, maximum: 500
 
-  after_create_commit { MessageBroadcastJob.perform_later(self) }
+  after_create_commit :broadcast_message
 
   def timestamp
     created_at.strftime('%H:%M:%S %d %B %Y')
+  end
+
+  def broadcast_message
+    ActionCable.server.broadcast "chat_rooms_#{chat_room_id}_channel",
+                                  message: render_message(self)
+  end
+
+  def render_message(message)
+    MessagesController.render partial: 'messages/message', locals: { message: message }
   end
 end
