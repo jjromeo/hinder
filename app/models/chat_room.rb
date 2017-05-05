b3a9e4533ea5617d282546018aa53e186a44e73f
Jerome Pratt <jerome.pratt@outlook.com>
2017-05-05 12:02:08 +0100

    Edit code causing bug with latest chat room

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -3,7 +3,7 @@ class ChatRoom < ApplicationRecord
   belongs_to :chat_receiver, class_name: 'User'
   has_many :messages, dependent: :destroy
 
-  scope :latest_for_user, ->(user) { where('chat_initiator_id = ? OR chat_receiver_id = ?', user.id, user.id).last }
+  scope :for_user, ->(user) { where('chat_initiator_id = ? OR chat_receiver_id = ?', user.id, user.id) }
 
   def other_user(user)
     user == chat_initiator ? chat_receiver : chat_initiator
